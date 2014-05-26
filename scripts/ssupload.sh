a800f294a2888f71514d3eab0bfc5fbc49588926
hahi <none@none.none>
2014-05-26 03:28:21 -0400

    fixed hotkey issue

--- scripts/ssupload.sh
+++ scripts/ssupload.sh
@@ -8,22 +8,24 @@ image_host="pomf.se"
 notification="dzen2"
 notification_args=(-w 400 -h 50 -x 20 -y 20 -fg '#282828' -bg '#ff99cc' -fn '-*-dejavu sans-medium-r-normal--13-*-*-*-*-*-iso10646-1' -p 3)
 
+sleep 1 # required to fix scrot -s issue that breaks keybinds
+
 file="$(scrot -s "${screenshot_dir}${filename_format}${file_extension}" -e 'echo -n $f')"
 
 if [[ $image_host == "pomf.se" ]]; then
     base="$(curl -sf -F files[]=@"$file" http://pomf.se/upload.php | grep -Eo "\"url\":\"[A-Za-z0-9]+${file_extension}\"," | sed 's/"url":"//;s/",//')"
 
-    if [[ -z "$base" ]]; then
-	if [[ $notification == "dzen2" ]]; then
-            echo "Error uploading image." | dzen2 "${notification_args[@]}"
-        else
-            echo "Error uploading image."
-	fi
+    url="http://a.pomf.se/$base"
+fi
 
-	exit 1
+if [[ -z "$base" ]]; then
+    if [[ $notification == "dzen2" ]]; then
+        echo "Error uploading image." | dzen2 "${notification_args[@]}"
+    else
+        echo "Error uploading image."
     fi
 
-    url="http://a.pomf.se/$base"
+    exit 1
 fi
 
 echo -n "$url" | xclip
