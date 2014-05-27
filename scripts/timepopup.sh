38c6ec44d67bd9ab7f1345d0b6f9569eb9cdaa3f
hahi <none@none.none>
2014-05-26 21:21:41 -0400

    added timepopup.sh

---
+++ scripts/timepopup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+popup_duration="5"
+
+for i in $(seq 1 ${popup_duration}); do \
+	LC_ALL="en-US.utf8" date +"%A %Y-%m-%d %H:%M:%S %Z"; \
+	LC_ALL="en-US.utf8" TZ="CET" date +"%A %Y-%m-%d %H:%M:%S %Z"; \
+	TZ="Japan" date +"%Y年%-m月%-d日（%a） %H:%M:%S %Z"; \
+	sleep 1; \
+done \
+| dzen2 -w 400 -h 50 -x 20 -y 20 -fg '#282828' -bg '#ff99cc' -fn '-*-ipapgothic-medium-r-normal--13-*-*-*-*-*-iso10646-1' -l 2 -u -sa c
