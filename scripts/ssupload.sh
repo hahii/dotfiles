0d85fac048a06afc02dae3395c3573d783bce091
hahi <none@none.none>
2015-11-27 01:55:01 -0500

    updated uguu api

--- scripts/ssupload.sh
+++ scripts/ssupload.sh
@@ -17,7 +17,7 @@ if [[ $image_host == "pomf.se" ]]; then
 
     url="http://a.pomf.se/$base"
 elif [[ $image_host == "uguu.se" ]]; then
-    base="$(curl -sf -F randomname=xxxxxx -F file=@"$file" http://uguu.se/api.php?d=upload-tool)"
+    base="$(curl -sf -F randomname=xxxxxx -F file=@"$file" https://uguu.se/api.php?d=upload-tool)"
 
     url="$base"
 fi
