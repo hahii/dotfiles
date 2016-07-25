2005369624f48b3144dd4498231861e35ec8e066
hahi <none@none.none>
2016-07-25 16:28:04 -0400

    updated remove_closed_captions.sh

--- scripts/remove_closed_captions.sh
+++ scripts/remove_closed_captions.sh
@@ -26,6 +26,6 @@
 # the file must NOT be CRLF format for this to work, so it is converted at the start of the script using s/\r$//
 
 for file in "$@"; do
-	sed -i 's/\r$//; s/((//g; s/))//g; s/♪♪～//g; s/♬～//g; s/♪～//g; s/[♪♬☎≪≫＜＞《》⦅⦆→➡]//g; s/([^)]*)//g; s/\[[^]]*\]//g; s/（[^）]*）//g' "$file"
+	sed -i 's/\r$//; s/((//g; s/))//g; s/♪♪～//g; s/♬～//g; s/♪～//g; s/[♪♬☎≪≫＜＞〈〉《》⦅⦆→➡]//g; s/([^)]*)//g; s/\[[^]]*\]//g; s/（[^）]*）//g' "$file"
 	sed -i '/^[0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9] --> [0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]$/{n;s/$/~+~+~/}; /^~+~+~/d; s/~+~+~//' "$file"
 done
