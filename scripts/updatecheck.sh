a278df98dd47ba352a0e6b5cb0a2409994dae1f9
hahi <none@none.none>
2018-04-13 15:36:56 -0400

    update file list changes

--- scripts/updatecheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo -n "Last system update: "; \
-grep 'starting full system upgrade' /var/log/pacman.log | tail -n 1 | tr -d []'[:alpha:]'; \
-echo; \
-echo "Available updates:"; \
-/usr/bin/checkupdates
